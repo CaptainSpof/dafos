7efebe09166f80a695ec29a8510474c4db8ae70c
CaptainSpof <captain.spof@gmail.com>
2026-09-20 11:55:16 +0200

    hass: remove donetick automation for now
    WIP, building from the GUI

--- modules/nixos/services/home-assistant/default.nix
+++ modules/nixos/services/home-assistant/default.nix
@@ -306,7 +306,7 @@ in
             filename = "ui-lovelace.yaml";
           };
 
-          "automation manual" = [ (import ./automations/donetick-chores.nix) ];
+          "automation manual" = [ ];
           "automation ui" = "!include automations.yaml";
           "scene ui" = "!include scenes.yaml";
           "script ui" = "!include_dir_merge_named scripts/";
