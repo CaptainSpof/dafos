cadadbf13f493dc153def01451206792da8e9b6e
CaptainSpof <captain.spof@gmail.com>
2026-09-21 10:29:32 +0200

    bookorbit: bump to v3.0.0

--- modules/home/services/bookorbit/default.nix
+++ modules/home/services/bookorbit/default.nix
@@ -48,7 +48,7 @@ in
       to `false` to keep BookOrbit LAN/tailnet-only.
     '';
 
-    image = mkOpt types.str "ghcr.io/bookorbit/bookorbit:2.10.0" "Container image to run.";
+    image = mkOpt types.str "ghcr.io/bookorbit/bookorbit:3.0.0" "Container image to run.";
     dbImage = mkOpt types.str "docker.io/pgvector/pgvector:pg18" ''
       Database image. BookOrbit needs the `uuid-ossp`, `pg_trgm` and `vector`
       extensions, which rules out a plain `postgres` image.
